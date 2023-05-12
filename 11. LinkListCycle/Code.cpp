2ba423b680c3e6f962e3be5f22ce0b542ef677fd
Tarun Kanti Jena <98694852+tkj2410@users.noreply.github.com>
2023-05-12 22:49:29 +0530

    Day 11

---
+++ 11. LinkListCycle/Code.cpp
@@ -0,0 +1,18 @@
+bool hasCycle(ListNode *head) {
+        if(head ==NULL || head->next==NULL){
+            return false;
+        }
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast!=NULL && fast->next !=NULL && fast->next->next!=NULL)
+        {
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast == slow)
+            {
+                return true;
+            }
+        }
+        return false;
+        
+    }
