7c4797a27b7db371d1bc596c950eb6a6d0229d32
Tarun Kanti Jena <98694852+tkj2410@users.noreply.github.com>
2023-08-21 22:41:15 +0530

    Day 98

---
+++ 98. Container With Most Water/Code.cpp
@@ -0,0 +1,16 @@
+    int maxArea(vector<int>& height) {
+        int n = height.size();
+        int left = 0, right = n - 1;
+        int max_area = 0;
+        while (left < right) {
+            int area = min(height[left], height[right]) * (right - left);
+            max_area = max(max_area, area);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max_area;
+        
+    }
